048674789b2ad66402f1258687dec8d47b48e5e4
Ritesh Verma <lrvevil@gmail.com>
2021-09-03 08:27:40 +0530

    Images added

--- src/chattingApplication/Server.java
+++ src/chattingApplication/Server.java
@@ -1,4 +1,16 @@
 package chattingApplication;
 
-public class Server {
+import javax.swing.*;
+
+public class Server extends JFrame {
+    Server(){
+        setSize(350,480);
+        setVisible(true);
+        setLocation(220,100);
+    }
+
+    public static void main(String[] args) {
+        new Server().setVisible(true);
+    }
+
 }
